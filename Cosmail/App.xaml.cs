238391a175ac4b8da56d365e0be1559f132248f7
Ondřej Kubíček <lordfanger@gmail.com>
2023-03-12 17:24:07 +0100

    Add project files.

---
+++ Cosmail/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Cosmail;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ Cosmail/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Cosmail;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
