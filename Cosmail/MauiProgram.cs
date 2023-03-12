9849bf23aedbd0f06d8c760b0e3f92c6c1b010dc
Ondřej Kubíček <lordfanger@gmail.com>
2023-03-12 17:48:20 +0100

    Strip default Blazor template

--- Cosmail/MauiProgram.cs
+++ Cosmail/MauiProgram.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.Logging;
-using Cosmail.Data;
 
 namespace Cosmail;
 
@@ -21,8 +20,6 @@ public static class MauiProgram
 		builder.Services.AddBlazorWebViewDeveloperTools();
 		builder.Logging.AddDebug();
 #endif
-
-		builder.Services.AddSingleton<WeatherForecastService>();
 
 		return builder.Build();
 	}
